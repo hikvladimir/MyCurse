cbb0839cdee7f8f7ace31d476df3e5f1f938b960
hikvladimir <psihouse@mail.ru>
2023-03-30 21:26:36 +0300

    run pages

--- src/main/java/org/CloneCurses/MainController.java
+++ src/main/java/org/CloneCurses/MainController.java
@@ -8,10 +8,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class MainController {
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
+  @GetMapping("/")
+    public String home(Model model) {
+      model.addAttribute("name", "World");
+      return "home";
+  }
+    @GetMapping("/about")
+    public String about(Model model) {
+        model.addAttribute("title", "О Нас");
+        return "about";
     }
 
 }
